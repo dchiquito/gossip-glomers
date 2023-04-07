a68819df73766652278418b53bb19843ee8fa01e
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-07 19:33:44 -0400

    Use the correct test invocation

--- unique-id-generation/test.sh
+++ unique-id-generation/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cargo build --release
-../maelstrom/maelstrom test -w unique-ids --bin target/release/echo --node-count 1 --time-limit 10
+../maelstrom/maelstrom test -w unique-ids --bin target/release/echo --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
