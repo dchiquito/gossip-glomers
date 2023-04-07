952c7b0706b5012641d9c25016174727e68275c3
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-07 19:14:22 -0400

    Make Message generic

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,17 +1,11 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use serde_json::{from_value, to_value, Map, Value};
+use serde_json::{to_value, Value};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Message {
+struct Message<T> {
     src: String,
     dest: String,
-    body: Map<String, Value>,
-}
-
-impl Message {
-    fn parse_body<T: DeserializeOwned>(&self) -> serde_json::Result<T> {
-        from_value(Value::Object(self.body.clone()))
-    }
+    body: T,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,14 +31,6 @@ impl InitOk {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Echo {
-    r#type: String,
-    msg_id: u32,
-    in_reply_to: Option<u32>,
-    echo: Option<String>,
-}
-
 struct Server {
     node_id: String,
     node_ids: Vec<String>,
@@ -56,44 +42,67 @@ impl Server {
             node_id: "".to_string(),
             node_ids: vec![],
         };
-        let message = server.read_message().unwrap();
-        let init: Init = message.parse_body().unwrap();
-        let init_ok = InitOk::new(&init);
-        server.node_id = init.node_id;
-        server.node_ids = init.node_ids;
-        server.respond(message, init_ok).unwrap();
+        let init: Message<Init> = server.read_message().unwrap();
+        let init_ok = InitOk::new(&init.body);
+        server.node_id = init.body.node_id.clone();
+        server.node_ids = init.body.node_ids.clone();
+        server.respond(&init, &init_ok).unwrap();
         server
     }
-    fn read_message(&self) -> serde_json::Result<Message> {
+    fn read_message<T: DeserializeOwned>(&self) -> serde_json::Result<Message<T>> {
         let stdin = std::io::stdin();
         let mut deserializer = serde_json::Deserializer::from_reader(stdin);
         Message::deserialize(&mut deserializer)
     }
 
-    fn write_message<T: Serialize>(&self, to: &str, body: T) -> serde_json::Result<()> {
-        let message = match to_value(body)? {
-            Value::Object(body_map) => Message {
-                src: self.node_id.clone(),
-                dest: to.to_string(),
-                body: body_map,
-            },
-            _ => panic!("Message body is not an object"),
-        };
-        println!("{}", serde_json::to_string(&message)?);
+    fn send_message<T: Serialize>(&self, message: &Message<T>) -> serde_json::Result<()> {
+        println!("{}", serde_json::to_string(message)?);
         Ok(())
     }
-    fn respond<T: Serialize>(&self, to: Message, body: T) -> serde_json::Result<()> {
-        self.write_message(&to.src, body)
+    fn send_body<T: Serialize>(&self, to: &str, body: &T) -> serde_json::Result<()> {
+        let message = Message {
+            src: self.node_id.clone(),
+            dest: to.to_string(),
+            body,
+        };
+        self.send_message(&message)
     }
+    /// Respond to a message. If the message has a msg_id, set the in_reply_to appropriately
+    fn respond<T: Serialize, U: Serialize>(
+        &self,
+        to: &Message<T>,
+        body: &U,
+    ) -> serde_json::Result<()> {
+        let original_body_map = match to_value(&to.body)? {
+            Value::Object(map) => map,
+            _ => panic!("Message body is not an object"),
+        };
+        if let Some(msg_id) = original_body_map.get("msg_id") {
+            let mut new_body_map = match to_value(body)? {
+                Value::Object(map) => map,
+                _ => panic!("Message body is not an object"),
+            };
+            new_body_map.insert("in_reply_to".to_string(), msg_id.clone());
+            self.send_body(&to.src, &new_body_map)
+        } else {
+            self.send_body(&to.src, body)
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Echo {
+    r#type: String,
+    msg_id: u32,
+    in_reply_to: Option<u32>,
+    echo: Option<String>,
 }
 
 fn main() -> serde_json::Result<()> {
     let server = Server::new();
     loop {
-        let message = server.read_message()?;
-        let mut echo: Echo = message.parse_body()?;
-        echo.r#type = "echo_ok".to_string();
-        echo.in_reply_to = Some(echo.msg_id);
-        server.respond(message, echo)?;
+        let mut message: Message<Echo> = server.read_message()?;
+        message.body.r#type = "echo_ok".to_string();
+        server.respond(&message, &message.body)?;
     }
 }
