6a9ee31156e316d84ce51f4e2f1e4d70e09eac62
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-04-08 12:06:52 -0400

    Epic refactor

--- broadcast-d/src/main.rs
+++ broadcast-d/src/main.rs
@@ -1,124 +1,84 @@
-use std::collections::HashSet;
-use std::{cell::RefCell, rc::Rc};
+use std::sync::{Arc, Mutex};
 
 use serde::{Deserialize, Serialize};
-use server::{Message, Server};
+use server::Message;
 
 mod server;
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Broadcast {
-    r#type: String,
-    msg_id: Option<u64>,
-    message: u64,
-}
-impl Broadcast {
-    fn new(message: u64) -> Broadcast {
-        Broadcast {
-            r#type: "broadcast".to_string(),
-            msg_id: None,
-            message,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct BroadcastOk {
-    r#type: String,
-}
-impl BroadcastOk {
-    fn new() -> BroadcastOk {
-        BroadcastOk {
-            r#type: "broadcast_ok".to_string(),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Read {
-    r#type: String,
-}
-impl Read {
-    fn new() -> Read {
-        Read {
-            r#type: "read".to_string(),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ReadOk {
-    r#type: String,
-    messages: Vec<u64>,
-}
-impl ReadOk {
-    fn new(messages: Vec<u64>) -> ReadOk {
-        ReadOk {
-            r#type: "read_ok".to_string(),
-            messages,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct TopologyOk {
-    r#type: String,
-}
-impl TopologyOk {
-    fn new() -> TopologyOk {
-        TopologyOk {
-            r#type: "topology_ok".to_string(),
-        }
-    }
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum P {
+    Broadcast {
+        #[serde(rename = "message")]
+        value: u64,
+    },
+    BroadcastOk {},
+    Read {},
+    ReadOk {
+        #[serde(rename = "messages")]
+        values: Vec<u64>,
+    },
+    Topology {},
+    TopologyOk {},
 }
 
 fn main() -> serde_json::Result<()> {
-    let mut server = Server::new();
-    let sender = server.sender.clone();
-    let adj_nodes_thread = sender.lock().unwrap().node_ids.clone();
+    let (server, sender) = server::init()?;
+    let mut values = vec![];
+    let adj_nodes = sender.node_ids.clone();
+    let adj_nodes_thread = adj_nodes.clone();
+    let sender = Arc::new(Mutex::new(sender));
+    let thread_sender = sender.clone();
     std::thread::spawn(move || loop {
         std::thread::sleep(std::time::Duration::from_secs(5));
         for adj_node in adj_nodes_thread.iter() {
-            sender
+            thread_sender
                 .lock()
                 .unwrap()
-                .send_body(adj_node, &Read::new())
+                .send(adj_node, &P::Read {})
                 .expect("Error sending refresh");
         }
     });
-    let adj_nodes = server.sender.lock().unwrap().node_ids.clone();
-    let messages: Rc<RefCell<Vec<u64>>> = Rc::new(RefCell::new(vec![]));
-    let messages_read = messages.clone();
-    let messages_read_ok = messages.clone();
-    server.handle("broadcast", move |sender, msg| {
-        let msg: Message<Broadcast> = msg.cast()?;
-        let message = msg.body.message;
-        let mut messages = messages.borrow_mut();
-        if !messages.contains(&message) {
-            messages.push(message);
-            for adj_node in adj_nodes.iter() {
-                sender.send_body(adj_node, &Broadcast::new(message))?;
+    loop {
+        let message: Message<P> = server.read_message()?;
+        match message.body.fields {
+            P::Broadcast { value } => {
+                if !values.contains(&value) {
+                    values.push(value);
+                    for adj_node in adj_nodes.iter() {
+                        sender
+                            .lock()
+                            .unwrap()
+                            .send(adj_node, &P::Broadcast { value })?;
+                    }
+                }
+                sender
+                    .lock()
+                    .unwrap()
+                    .respond(&message, &P::BroadcastOk {})?
             }
-        }
-        sender.respond(&msg, &BroadcastOk::new())
-    });
-    server.handle("broadcast_ok", move |_, _| Ok(()));
-    server.handle("read", move |sender, msg| {
-        sender.respond(msg, &ReadOk::new(messages_read.borrow().to_vec()))
-    });
-    server.handle("read_ok", move |_, msg| {
-        let msg: Message<ReadOk> = msg.cast()?;
-        for value in msg.body.messages {
-            let mut messages = messages_read_ok.borrow_mut();
-            if !messages.contains(&value) {
-                messages.push(value);
+            P::BroadcastOk {} => {}
+            P::Read {} => sender.lock().unwrap().respond(
+                &message,
+                &P::ReadOk {
+                    values: values.clone(),
+                },
+            )?,
+            P::ReadOk {
+                values: read_values,
+            } => {
+                for value in read_values {
+                    if !values.contains(&value) {
+                        values.push(value);
+                    }
+                }
             }
+            P::Topology {} => sender
+                .lock()
+                .unwrap()
+                .respond(&message, &P::TopologyOk {})?,
+            P::TopologyOk {} => {}
         }
-        Ok(())
-    });
-    server.handle("topology", move |sender, msg| {
-        // Just ignore the topology lmao
-        sender.respond(msg, &TopologyOk::new())
-    });
-    server.serve()
+    }
 }
--- broadcast-d/src/server.rs
+++ broadcast-d/src/server.rs
@@ -1,227 +1,131 @@
-use std::{
-    collections::{hash_map::DefaultHasher, HashMap},
-    sync::{Arc, Mutex},
-    time::Instant,
-};
+use std::{collections::hash_map::DefaultHasher, io::Write};
 
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use serde_json::{from_value, to_value, Map, Result, Value};
+use serde_json::Result;
 use std::hash::{Hash, Hasher};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Message<T> {
     pub src: String,
     pub dest: String,
-    pub body: T,
-}
-
-impl Message<Map<String, Value>> {
-    pub fn cast<T: DeserializeOwned>(&self) -> Result<Message<T>> {
-        let body = from_value(serde_json::Value::Object(self.body.clone()))?;
-        Ok(Message {
-            src: self.src.clone(),
-            dest: self.src.clone(),
-            body,
-        })
-    }
+    pub body: Body<T>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Body<T> {
-    pub src: String,
-    pub dest: String,
-    pub body: T,
+    pub msg_id: Option<u64>,
+    pub in_reply_to: Option<u64>,
+    #[serde(flatten)]
+    pub fields: T,
 }
 
-// #[derive(Serialize, Deserialize, Debug, Clone)]
-// pub struct Body<T> {
-//     pub msg_id: u64,
-//     pub in_reply_to: u64,
-//     #[serde(flatten)]
-//     pub fields: T,
-// }
-//
-// #[derive(Serialize, Deserialize, Debug, Clone)]
-// #[serde(tag = "type")]
-// #[serde(rename_all = "snake_case")]
-// pub enum Payload {
-//     ...
-// }
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Init {
-    r#type: String,
-    msg_id: u32,
-    node_id: String,
-    node_ids: Vec<String>,
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type")]
+#[serde(rename_all = "snake_case")]
+enum InitPayload {
+    Init {
+        node_id: String,
+        node_ids: Vec<String>,
+    },
+    InitOk {},
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct InitOk {
-    r#type: String,
-    in_reply_to: u32,
-}
-
-impl InitOk {
-    fn new(init: &Init) -> InitOk {
-        InitOk {
-            r#type: "init_ok".to_string(),
-            in_reply_to: init.msg_id,
-        }
-    }
-}
-
-type Handler = dyn FnMut(&mut Sender, &Message<Map<String, Value>>) -> Result<()>;
-
-pub struct Server {
-    pub sender: Arc<Mutex<Sender>>,
-    handlers: HashMap<String, Box<Handler>>,
-}
+pub struct Server {}
 
 impl Server {
-    pub fn new() -> Server {
-        let server = Server {
-            sender: Arc::new(Mutex::new(Sender {
-                node_id: "".to_string(),
-                node_ids: vec![],
-                counter: 0,
-                // pending_rpcs: HashMap::new(),
-            })),
-            handlers: HashMap::new(),
-        };
-        let init: Message<Init> = server.read_message().unwrap();
-        server.sender.lock().unwrap().init(&init);
-        let init_ok = InitOk::new(&init.body);
-        server
-            .sender
-            .lock()
-            .unwrap()
-            .respond(&init, &init_ok)
-            .unwrap();
-        server
+    fn init() -> (Server, Message<InitPayload>) {
+        let server = Server {};
+        let init_message: Message<InitPayload> = server.read_message().unwrap();
+        eprintln!("initin {:?}", init_message);
+        (server, init_message)
     }
     pub fn read_message<T: DeserializeOwned>(&self) -> Result<Message<T>> {
-        let stdin = std::io::stdin();
+        let stdin = std::io::stdin().lock();
         let mut deserializer = serde_json::Deserializer::from_reader(stdin);
         Message::deserialize(&mut deserializer)
     }
-    pub fn handle<F>(&mut self, r#type: &str, handler: F)
-    where
-        F: FnMut(&mut Sender, &Message<Map<String, Value>>) -> Result<()> + 'static,
-    {
-        self.handlers.insert(r#type.to_string(), Box::new(handler));
-    }
-    pub fn serve(&mut self) -> serde_json::Result<()> {
-        loop {
-            let message: Message<Map<String, Value>> = self.read_message()?;
-            eprintln!("Handling {:?}", message);
-            // Check if we are waiting for a response for that msg_id
-            // if let Some(Value::Number(in_reply_to)) = message.body.get("in_reply_to") {
-            //     eprintln!("gotta replyto {:?}", in_reply_to);
-            //     if let Some((_, _, mut handler)) = self
-            //         .sender
-            //         .pending_rpcs
-            //         .remove(&in_reply_to.as_u64().unwrap())
-            //     {
-            //         eprintln!("gotta rhandle ");
-            //         handler(&mut self.sender, &message)?;
-            //         continue;
-            //     }
-            // }
-            // Check if there is a valid handler
-            if let Some(handler) = self.handlers.get_mut(
-                message
-                    .body
-                    .get("type")
-                    .expect("Message had no type")
-                    .as_str()
-                    .expect("Message type was not a string"),
-            ) {
-                handler(&mut self.sender.lock().unwrap(), &message)?;
-                continue;
-            }
-            panic!("No handler for {:?}", message);
-        }
-    }
 }
 
-// type PendingMessage = (Instant, Message<Map<String, Value>>, Box<Handler>);
-
+#[derive(Clone)]
 pub struct Sender {
     pub node_id: String,
     pub node_ids: Vec<String>,
     counter: u64,
-    // pending_rpcs: HashMap<u64, PendingMessage>,
 }
 
 impl Sender {
-    fn init(&mut self, init: &Message<Init>) {
-        self.node_id = init.body.node_id.clone();
-        self.node_ids = init.body.node_ids.clone();
-        let mut hasher = DefaultHasher::new();
-        self.node_id.hash(&mut hasher);
-        self.counter = hasher.finish();
+    fn init(init_message: &Message<InitPayload>) -> Result<Sender> {
+        let mut sender = match &init_message.body.fields {
+            InitPayload::Init { node_id, node_ids } => {
+                let mut hasher = DefaultHasher::new();
+                node_id.hash(&mut hasher);
+                Sender {
+                    node_id: node_id.clone(),
+                    node_ids: node_ids.clone(),
+                    counter: hasher.finish(),
+                }
+            }
+            _ => panic!("Invalid init message"),
+        };
+        let init_ok = InitPayload::InitOk {};
+        eprintln!("blastin {:?}", init_ok);
+        sender.respond(init_message, init_ok)?;
+        Ok(sender)
+    }
+    /// Write a message directly to stdout
+    pub fn send_message<T: Serialize>(&self, message: &Message<T>) -> Result<()> {
+        let stdout = std::io::stdout().lock();
+        let mut serializer = serde_json::Serializer::new(stdout);
+        message.serialize(&mut serializer)?;
+        serializer
+            .into_inner()
+            .write_all(b"\n")
+            .expect("Error writing newline");
+        Ok(())
     }
     /// Adds the msg_id field to a body and wraps it in a Message
-    pub fn message<T: Serialize>(
-        &mut self,
-        to: &str,
-        body: T,
-    ) -> Result<Message<Map<String, Value>>> {
-        let mut body_map = match to_value(body)? {
-            Value::Object(map) => map,
-            _ => panic!("Message body is not an object"),
-        };
+    pub fn message<T: Serialize>(&mut self, to: &str, fields: T) -> Result<Message<T>> {
         let msg_id = self.counter;
         self.counter += 1;
-        body_map.insert("msg_id".to_string(), to_value(msg_id)?);
+        let body = Body {
+            msg_id: Some(msg_id),
+            in_reply_to: None,
+            fields,
+        };
         Ok(Message {
             src: self.node_id.clone(),
             dest: to.to_string(),
-            body: body_map,
+            body,
         })
     }
-    /// Write a message to stdout. msg_id will not be included
-    pub fn send_message<T: Serialize>(&self, message: &Message<T>) -> Result<()> {
-        println!("{}", serde_json::to_string(message)?);
-        Ok(())
+    /// Creates a response to a message by setting the msg_id and in_reply_to fields
+    pub fn response<T, U>(&mut self, to: &Message<T>, fields: U) -> Result<Message<U>>
+    where
+        T: Serialize,
+        U: Serialize,
+    {
+        let mut message = self.message(&to.src, fields)?;
+        message.body.in_reply_to = to.body.msg_id;
+        Ok(message)
     }
     /// Send a message body to stdout
-    pub fn send_body<T: Serialize>(&mut self, to: &str, body: T) -> Result<()> {
-        let message = self.message(to, body)?;
+    pub fn send<T: Serialize>(&mut self, to: &str, fields: T) -> Result<()> {
+        let message = self.message(to, fields)?;
         self.send_message(&message)
     }
     /// Respond to a message. If the message has a msg_id, set the in_reply_to appropriately
-    pub fn respond<T: Serialize, U: Serialize>(&mut self, to: &Message<T>, body: &U) -> Result<()> {
-        let original_body_map = match to_value(&to.body)? {
-            Value::Object(map) => map,
-            _ => panic!("Message body is not an object"),
-        };
-        if let Some(msg_id) = original_body_map.get("msg_id") {
-            let mut new_body_map = match to_value(body)? {
-                Value::Object(map) => map,
-                _ => panic!("Message body is not an object"),
-            };
-            new_body_map.insert("in_reply_to".to_string(), msg_id.clone());
-            eprintln!("RESPONSIN {:?}", new_body_map);
-            self.send_body(&to.src, &new_body_map)
-        } else {
-            self.send_body(&to.src, body)
-        }
+    pub fn respond<T: Serialize, U: Serialize>(
+        &mut self,
+        to: &Message<T>,
+        fields: U,
+    ) -> Result<()> {
+        let message = self.response(to, fields)?;
+        self.send_message(&message)
     }
-    // TODO rpc callbacks are not thread safe
-    // pub fn rpc<T: Serialize, F>(&mut self, to: &str, body: &T, callback: F) -> Result<()>
-    // where
-    //     F: FnMut(&mut Sender, &Message<Map<String, Value>>) -> Result<()> + 'static,
-    // {
-    //     let message = self.message(to, body)?;
-    //     self.pending_rpcs.insert(
-    //         message.body.get("msg_id").unwrap().as_u64().unwrap(),
-    //         (Instant::now(), message.clone(), Box::new(callback)),
-    //     );
-    //     self.send_message(&message)
-    // }
+}
 
-    // TODO retry unacked messages?
-    // pub fn check_for_timeouts() {}
+pub fn init() -> Result<(Server, Sender)> {
+    let (server, init_message) = Server::init();
+    let sender = Sender::init(&init_message)?;
+    Ok((server, sender))
 }
